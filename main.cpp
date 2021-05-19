93e068da158b139a8cfc6e51e98738ffdc33f958
Alejandro Suárez <alejandro-hdez115@outlook.com>
2021-05-19 23:39:36 +0000

    Finish class: Platillo

--- main.cpp
+++ main.cpp
@@ -12,6 +12,40 @@ class Platillo {
     string categoria;
     string preparacion;
   public:
+    // Setters
+    void set_id(int id)
+    {
+      this->id = id;
+    }
+    void set_nombre(string nombre)
+    {
+      this->nombre = nombre;
+    }
+    void set_costo_restaurante(float costo_restaurante)
+    {
+      this->costo_restaurante = costo_restaurante;
+    }
+    void set_costo_restaurante(float costo_comensal)
+    {
+      this->costo_comensal = costo_comensal;
+    }
+    void set_costo_restaurante(float tiempo_preparacion)
+    {
+      this->tiempo_preparacion = tiempo_preparacion;
+    }
+    void set_temporada(string temporada)
+    {
+      this->temporada = temporada;
+    }
+    void set_categoria(string categoria)
+    {
+      this->categoria = categoria;
+    }
+    void set_preparacion(string preparacion)
+    {
+      this->preparacion = preparacion;
+    }
+    // Getters
     int get_id()
     {
       return id;
